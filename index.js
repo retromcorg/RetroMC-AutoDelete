90d99b37ea6b70cce50337a999dc9948342a1a1d
1blckhrt <williams.1691@wright.edu>
2024-02-15 13:05:23 -0500

    fixing new deletion function

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Base files were coded by MusicMaker
 
-// setting up all handlers, client, and loading all commands and events
+// setting up all handlers, client, loading all commands and events, caching the auto delete channel, and logging in
 require("./utils/ProcessHandlers.js")();
 const { Client, PermissionFlagsBits, ActivityType } = require("discord.js");
 
@@ -22,12 +22,14 @@ client.logs = require("./utils/Logs.js");
 require("./utils/Loader.js")(client);
 require("./utils/Register.js")(client);
 
+const { autoDeleteChannel } = require("./config.json");
+
 client.logs.info(`Logging in...`);
 client.login(client.config.TOKEN);
 client.on("ready", function () {
   client.logs.success(`Logged in as ${client.user.tag}!`);
-  // setting interval to delete all non-bot messages every hour in the case the bot crashes
-  setInterval(deleteMessages, 3600000);
+  // setting interval to delete all non-bot messages every 4 hours in the case the bot crashes
+  setInterval(deleteMessages, 14400000);
 });
 
 // handle slash commands
@@ -83,20 +85,19 @@ client.on("interactionCreate", async function (interaction) {
 });
 
 // auto delete messages after 30 seconds
-const { autoDeleteChannel } = require("./config.json");
 client.on("messageCreate", async function (message) {
   if (message.author.bot) return;
   if (message.channel.type === "DM") return;
-  if (message.channelId !== autoDeleteChannel) return;
+  if (message.channelId !== client.config.autoDeleteChannel) return;
 
   setTimeout(() => {
     message.delete().catch(console.error);
   }, 30000);
 });
 
-// delete any messages not caught by the auto delete, such as messages sent before the bot was started, every hour
+// delete any messages not caught by the auto delete, such as messages sent before the bot was started, every 4 hours
 async function deleteMessages() {
-  const channel = await client.channels.fetch(autoDeleteChannel);
+  const channel = await client.channels.fetch(client.config.autoDeleteChannel);
   if (!channel) return console.error("Channel not found.");
 
   channel.messages
@@ -107,8 +108,14 @@ async function deleteMessages() {
       // Check if there are user messages to delete
       if (userMessages.size > 0) {
         // Delete all user messages
-        userMessages.forEach((msg) => {
-          msg.delete().catch(console.error);
+        userMessages.forEach(async (msg) => {
+          try {
+            await msg.delete();
+          } catch (error) {
+            console.error(
+              `Error deleting message: ${error.message}. This can safely be ignored.`
+            );
+          }
         });
         console.log(`${userMessages.size} user messages deleted.`);
       } else {
