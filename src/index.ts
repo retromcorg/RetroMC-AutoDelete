e427c35d85ceaf6b528999d13938df9854c0151c
1blckhrt <williams.1691@wright.edu>
2025-04-11 20:26:22 -0400

    feat: add client, command/event file loaders

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log("hello world");
+import { fileURLToPath, URL } from "node:url";
+import { Client, Collection, GatewayIntentBits } from "discord.js";
+import { isCommand, isEvent } from "./types/index.js";
+import { loadStructures } from "./util/helpers.js";
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages],
+});
+
+client.commands = new Collection();
+
+const commands = await loadStructures(fileURLToPath(new URL("commands", import.meta.url)), isCommand);
+
+for (const command of commands) {
+  client.commands.set(command.data.name, command);
+}
+
+const events = await loadStructures(fileURLToPath(new URL("events", import.meta.url)), isEvent);
+
+for (const event of events) {
+  client[event.once ? "once" : "on"](event.name, async (...args) => event.execute(...args));
+}
+
+await client.login();
