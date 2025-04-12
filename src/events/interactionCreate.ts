707ab7183c6e0afde828e33b998661670ca50791
1blckhrt <williams.1691@wright.edu>
2025-04-11 20:43:25 -0400

    feat: add base events

---
+++ src/events/interactionCreate.ts
@@ -0,0 +1,38 @@
+import type { CommandInteraction } from "discord.js";
+import { Events } from "discord.js";
+import type { Event } from "../types/event.js";
+
+export async function handleCommandInteraction(interaction: CommandInteraction): Promise<void> {
+  const command = interaction.client.commands.get(interaction.commandName);
+
+  if (!command) {
+    console.warn(`The command ${interaction.commandName} is not loaded but was used.`);
+
+    await interaction.reply({
+      content: "Invalid command, this wasn't supposed to happen.",
+      ephemeral: true,
+    });
+
+    return;
+  }
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+
+    await interaction.reply({
+      content: "An unknown error occurred, please try again later.",
+      ephemeral: true,
+    });
+  }
+}
+
+export default {
+  name: Events.InteractionCreate,
+  async execute(interaction) {
+    if (interaction.isCommand()) {
+      await handleCommandInteraction(interaction);
+    }
+  },
+} as const satisfies Event<Events.InteractionCreate>;
